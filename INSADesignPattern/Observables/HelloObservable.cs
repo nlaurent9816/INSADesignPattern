6c742ff9931867eef014106d83dd8bf0d3d47b79
Nicolas LAURENT <nicolasLaurent9816@gmail.com>
2017-10-03 18:30:17 +0200

    Réajout de modifications non enregistrées lors de la création de la branche.

--- INSADesignPattern/Observables/HelloObservable.cs
+++ INSADesignPattern/Observables/HelloObservable.cs
@@ -8,27 +8,24 @@ namespace INSADesignPattern.Observables
     {
 
         HelloContext monContexte;
+        IHelloStraegy usedStrat;
 
         //Constructeur
         //Récupère le contexte utilisé
         public HelloObservable(HelloContext helloContext)
         {
             monContexte = helloContext;
+            usedStrat = new HelloStratNoName();
         }
 
         public Boolean Execute()
         {
             int contextCounter;
-            string name;
-            IHelloStraegy usedStrat;
-
-            //Demande le nom
-            Console.WriteLine("What's your name ?");
-            name = Console.ReadLine();
+            
 
             //On donne le nom reçu au contexte qui va nous dire combien de fois de suite
             //le nom donné a été utilisé.
-            contextCounter = monContexte.GetContext(name);
+            contextCounter = monContexte.GetContext();
 
             //On choisit la stratégie selon le contexte
             if (contextCounter < 1)
@@ -41,7 +38,7 @@ namespace INSADesignPattern.Observables
                 usedStrat = new HelloStratC();
 
             //on exécute la stratégie
-            return usedStrat.RunStrategy(name);
+            return usedStrat.RunStrategy(monContexte.CurrentName);
 
         }
 
