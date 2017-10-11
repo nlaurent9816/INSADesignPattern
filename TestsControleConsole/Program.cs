460b037fc3dfa43b0f4d72da586c7096b7cdc398
Nicolas LAURENT <nicolasLaurent9816@gmail.com>
2017-10-11 13:01:33 +0200

    Ajout du projet TestsControleConsole Petit projet pour tester comment contrôler l'affichage en console de commande.

---
+++ TestsControleConsole/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsControleConsole
+{
+    class Program
+    {
+
+        static private string generateBorder(int width, int height)
+        {
+            string result = "";
+            string upDownLines = "";
+            string otherLines = "";
+
+            width--;
+            height--;
+
+            for (int i = 0; i < width; i++)
+            {
+                upDownLines += "_";
+
+                if ((i == 0) || (i == width - 1))
+                    otherLines += "|";
+                else
+                    otherLines += " ";
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                if ((i == 0) || (i == height - 1))
+                    result += upDownLines + "\n";
+                else
+                    result += otherLines + "\n";
+
+            }
+
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            string border = generateBorder(50,20);
+            ConsoleKeyInfo pressedKey;
+
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.Title="Tests de contrôle de la console";
+
+            Console.SetWindowSize(50, 20);
+            Console.SetBufferSize(50,20);
+            Console.Write(border);
+
+            do
+            {
+                pressedKey = Console.ReadKey(true);
+                Console.Clear();
+                Console.Write(border);
+                Console.SetCursorPosition(1, 9);
+                Console.WriteLine("Vous avez tapé sur " + pressedKey.Key.ToString());
+
+            } while (pressedKey.Key != ConsoleKey.Escape);
+
+            
+
+
+
+
+
+            
+
+
+        }
+    }
+}
